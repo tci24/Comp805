0ce8a0395a44903c6f22ed08779157356993eb27
tiffany <tci24@wildcats.unh.edu>
2018-02-04 14:04:48 -0500

    updated 4 url patterns in urls.py to match newly created views

--- django/week2/portfolio/urls.py
+++ django/week2/portfolio/urls.py
@@ -19,5 +19,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
+    path('resume/', views.resume, name='resume'),
+    path('portfolio/', views.portfolio, name='portfolio'),
+    path('contact/', views.contact, name='contact'),
     path('admin/', admin.site.urls),
 ]
