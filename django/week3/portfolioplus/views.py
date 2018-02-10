d74295b0a5224ab4c11b24daade4f5bcad2e2c28
tiffany <tci24@wildcats.unh.edu>
2018-02-10 11:24:31 -0500

    start week3 django project

---
+++ django/week3/portfolioplus/views.py
@@ -0,0 +1,50 @@
+"""
+portfolioplus/views.py for the portfolio Python package in week 2 container
+Tiffany
+02/10/2018
+"""
+
+from django.shortcuts import render
+
+def home(request):
+	"""
+	Takes request of type HttpRequest and calls render() with ustudy.html file and empty context.
+	request: has information about current request that triggered this view
+	Returns: home page of week 2 portfolio site
+	"""
+	
+	context = {}
+	return render(request, 'home.html', context)
+
+
+def resume(request):
+	"""
+	Takes request of type HttpRequest and calls render() with ustudy.html file and empty context.
+	request: has information about current request that triggered this view
+	Returns: resume page of week 2 portfolio site
+	"""
+
+	context = {}
+	return render(request, 'resume.html', context)
+
+
+def portfolio(request):
+	"""
+	Takes request of type HttpRequest and calls render() with ustudy.html file and empty context.
+	request: has information about current request that triggered this view
+	Returns: portfolio page of week 2 portfolio site
+	"""
+
+	context = {}
+	return render(request, 'portfolio.html', context)
+
+
+def contact(request):
+	"""
+	Takes request of type HttpRequest and calls render() with ustudy.html file and empty context.
+	request: has information about current request that triggered this view
+	Returns: contact page of week 2 portfolio site
+	"""
+
+	context = {}
+	return render(request, 'contact.html', context)
