d5dcdd9eeb695ff9bea65fe7012ec30225a3e3fb
tiffany <tci24@wildcats.unh.edu>
2018-02-10 13:39:19 -0500

    created Experience model

--- django/week3/resume/models.py
+++ django/week3/resume/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Experience(models.Model):
+	title = models.CharField(max_length=60, null=False, blank=False)
+	location = models.CharField(max_length=60, null=False, blank=True)
+	start_date = models.DateField(null=True, blank=True)
+	end_date = models.DateField(null=True, blank=True)
+	description = models.TextField(null=False, blank=True)
