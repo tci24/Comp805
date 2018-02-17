425fc4605489923c80500afb22ca392e39783560
tiffany <tci24@wildcats.unh.edu>
2018-02-17 15:35:05 -0500

    modified lab3 to practice with doctest

--- labs/week3/lab3.py
+++ labs/week3/lab3.py
@@ -1,13 +1,18 @@
 """
-lab2.py
+lab3.py
 Tiffany
-01/30/2018
+02/06/2018
 """
 def squared(num_list):
 	"""
 	Squares numbers in num_list
 	num_list: list of numbers
 	Returns: list of these numbers squared
+
+	>>> squared([1, 2, 3])
+	[1, 4, 9]
+	>>> squared([-1, -2])
+	[1, 4]
 	"""
 
 	new_list = [] #initialize list to hold result
@@ -22,13 +27,18 @@ def check_title(title_list):
 	Removes strings in title_list that have numbers and aren't title case
 	title_list: list of strings
 	Returns: list of strings that are titles
+
+	>>> check_title(['Hi', 'puppy', 'Dog'])
+	['Hi', 'Dog']
 	"""
 
 	new_list = [ ]
+	'''
 	for title in title_list:
-        if title.istitle( ) and title.isalpha( ):
-            	new_list.append(title)
-    return new_list
+		if title.istitle( ) and title.isalpha( ):
+				new_list.append(title)
+	'''
+	return new_list
 
 def restock_inventory(inventory):
 	"""
@@ -37,11 +47,17 @@ def restock_inventory(inventory):
 		key: string that is the name of the inventory item
 		value: integer that equeals the number of that item currently on hand
 	Returns: updated dictionary where each inventory item is restocked
+	
+	>>> restock_inventory({'cherry':30, 'lemon':5})
+	{'cherry':40, 'lemon':15}
 	"""
-    new_inventory = { }
-    for key, val in inventory.items( ):
-        new_inventory[key] = val + 10
-    return new_inventory
+	
+	new_inventory = { }
+	'''
+	for key, val in inventory.items( ):
+		new_inventory[key] = val + 10
+	'''
+	return new_inventory
 
 def filter_0_items(inventory):
 	"""
@@ -50,12 +66,18 @@ def filter_0_items(inventory):
 		key: string that is the name of the inventory item
 		value: integer that equeals the number of that item currently on hand
 	Returns: the same inventory_dict with any item that had 0 quantity removed
+
+	>>> filter_0_items({'cherry':30, 'lemon':5, 'pear':0})
+	{'cherry':30, 'lemon':5}
 	"""
+
 	new_inventory = { }
-    for k, v in inventory.items( ):
-        if v != 0:
-            new_inventory[k] = v
-    return new_inventory
+	'''
+	for k, v in inventory.items( ):
+		if v != 0:
+			new_inventory[k] = v
+	'''
+	return new_inventory
 
 def average_grades(grades):
 	"""
@@ -64,8 +86,18 @@ def average_grades(grades):
 		key: string of student names
 		value: list of integer grades received in class
 	Returns: dictionary that averages out the grades of each student
+	
+	>>> average_grades({'Michael':[100, 78, 88, 900/10], 'Daniel':[80, 95, 77, 64.0], 'Josh':[99, 89, 94, 66]})
+	{'Josh':87.0, 'Daniel':79.0, 'Michael':89.0}
 	"""
+
 	new_grades = { }
-    for student_name, student_grades in grades.items( ):
-    	new_grades[student_name] = sum(student_grades)/len(student_grades)
-    return new_grades
+	'''
+	for student_name, student_grades in grades.items( ):
+		new_grades[student_name] = sum(student_grades)/len(student_grades)
+	'''
+	return new_grades
+
+if __name__ =='__main__':
+	import doctest
+	doctest.testmod( )
