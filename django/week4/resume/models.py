5f7c2a16fc6fd377b6f98e590f6e23dd1bacd1dd
tiffany <tci24@wildcats.unh.edu>
2018-02-18 10:11:00 -0500

    created Resume model and added new resume object to database

--- django/week4/resume/models.py
+++ django/week4/resume/models.py
@@ -1,7 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Resume(models.Model):
+	first_name = models.CharField(max_length=25, null=False, blank=False)
+	last_name = models.CharField(max_length=25, null=True, blank=True)
+
+	def __str__(self):
+		return self.last_name
+
+
 class Experience(models.Model):
+	parent_resume = models.ForeignKey('Resume', on_delete=models.CASCADE, default=1)
 	title = models.CharField(max_length=60, null=False, blank=False)
 	location = models.CharField(max_length=60, null=False, blank=True)
 	start_date = models.DateField(null=True, blank=True)
@@ -12,6 +22,7 @@ class Experience(models.Model):
 		return self.title
 
 class Education(models.Model):
+	parent_resume = models.ForeignKey('Resume', on_delete=models.CASCADE, default=1)
 	institution_name = models.CharField(max_length=60, null=False, 
 		blank=False)
 	location = models.CharField(max_length=60, null=False, blank=True)
